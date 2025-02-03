f48912be566f354635ffe96ba9fc409ac95bba6b
Abhinav <abhinavhooda223@gmail.com>
2025-02-03 11:00:45 +0530

    schema setup done

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("hello world!");
+import {Telegraf} from 'telegraf';
+import userModel from './src/models/user.js'
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start(async (ctx) => {
+
+    const from = ctx.update.message.from;
+    console.log(from);
+
+    
+    // store the user information into db
+    await ctx.reply("welcome to drop post bot! 🚀")
+});
+
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
