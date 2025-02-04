2392b6aba579490f30d2c36046d88108f689c1e6
Abhinav <abhinavhooda223@gmail.com>
2025-02-04 23:51:06 +0530

    connect mongoose

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ bot.start(async (ctx) => {
 
     const from = ctx.update.message.from;
     console.log(from);
-
-    
-    // store the user information into db
-    await ctx.reply("welcome to drop post bot! 🚀")
+    try{
+        await userModel.findOneAndUpdate({tgId: from.id},{
+            $setOnInsert: {
+                firstName: from.first_name,
+                lastName: from.last_name,
+                isBot: from.is_bot,
+                tgId: from.id
+            }
+        },{upsert:true, new:true});
+        // store the user information into db
+        await ctx.reply("Hi!, ${from.first_name}, Welcome. I will be writing highly engaging social media posts for you Just keep feeding me with the events throught the day . Lets shine on social media ")
+    }catch(err){
+        console.log(err);
+        await ctx.reply("Error occurred while storing user data. Please try again later.");
+    }
 });
 
 
